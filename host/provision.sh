35b0603d8e30859319aa08c3095ee6109f367b80
Lincoln Stoll <lincoln.stoll@gmail.com>
2015-09-04 23:53:12 +0000

    remove that garbage

--- host/provision.sh
+++ host/provision.sh
@@ -4,7 +4,7 @@ set -e
 
 apt-get update
 apt-get upgrade
-apt-get install -y dnsmasq
+apt-get install -y dnsmasq node nodejs npm nodejs-legacy
 
 if [ ! -f /usr/bin/docker ]; then
     curl -sSL https://get.docker.com/ | sh
