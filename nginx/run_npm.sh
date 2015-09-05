e696f5158d6435caf3267ad7f23e84abb4bf55b4
Lincoln Stoll <lincoln.stoll@gmail.com>
2015-09-05 03:43:14 +0000

    Container defines it, so have to map it

--- nginx/run_npm.sh
+++ nginx/run_npm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d --name=npm-nginx -v $(pwd)/npm/nginx.conf:/etc/nginx/nginx.conf:ro -v /data/npm:/data/npm -p 172.16.51.241:80:80 nginx
+docker run -d --name=npm-nginx -v $(pwd)/npm/nginx.conf:/etc/nginx/nginx.conf:ro -v /data/npm:/data/npm -p 172.16.51.241:80:80 -p 172.16.51.241:443:443 nginx
