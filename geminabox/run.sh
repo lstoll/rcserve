b1421baf0783ebb29ac53d9e2fb6f8f426171bee
Lincoln Stoll <lincoln.stoll@gmail.com>
2015-09-05 04:34:36 +0000

    Make them always start

--- geminabox/run.sh
+++ geminabox/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d --name geminabox -v /data/rubygems:/data/rubygems -p 172.16.51.240:80:8989 geminabox
+docker run -d --name geminabox  --restart=always -v /data/rubygems:/data/rubygems -p 172.16.51.240:80:8989 geminabox
