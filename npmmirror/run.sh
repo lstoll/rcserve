b6b23405c61eecd4e398cefa0c6d1e39df2b1d5d
Lincoln Stoll <lincoln.stoll@gmail.com>
2015-09-08 20:56:28 +0000

    Change the tld

--- npmmirror/run.sh
+++ npmmirror/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -i -v /data/npm:/data/npm -v /data/npm_tmp:/data/npm_tmp npmmirror
+docker run --rm -i -v /data/npm:/data/npm -v /data/npm_tmp:/data/npm_tmp npmmirror registry-static -d npm.camp -o /data/npm -t /data/npm_tmp %@
